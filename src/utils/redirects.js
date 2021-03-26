17cf6770b4c9105e75cd6cf04d7e054fb2f1e8d0
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2021-03-26 10:55:51 +0000

    Add cookies and preview logic

---
+++ src/utils/redirects.js
@@ -0,0 +1,16 @@
+import { isEmpty } from "lodash";
+
+export const getPreviewRedirectUrl = (postType = "", previewPostId = "") => {
+  if (isEmpty(postType) || isEmpty(previewPostId)) {
+    return "";
+  }
+
+  switch (postType) {
+    case "post":
+      return "/blog/preview/${previewPostId}";
+    case "page":
+      return "/page/preview/${previewPostId}";
+    default:
+      return "/";
+  }
+};
